b9e27c6f508b3094306b4bcc67e60a28503ee0de
alida <101364345+alida42@users.noreply.github.com>
2022-06-21 15:36:06 +0200

    Replaced Current weather API with OneCall API

--- src/app.js
+++ src/app.js
@@ -32,10 +32,10 @@ function formatDay(timestamp) {
 }
 
 
-function displayForecast () {
+function displayForecast (response) {
     let forecastElement = document.querySelector("#forecast");
-
     let forecastHTML = `<div class="row">`;
+
     let days = [`Thu`, `Fri`, `Sat`, `Sun`];
     days.forEach(function(day)
     {
@@ -61,15 +61,6 @@ function displayForecast () {
         forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
 }
-
-
-
-
-
-
-
-
-
 
 
 // function displayForecast(response) {
@@ -110,7 +101,7 @@ function displayForecast () {
 
 function getForecast(coordinates) {
     let apiKey = `d59d21823b0a48106ca99bf9e0c71303`;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
 }
 
@@ -131,14 +122,14 @@ function displayTemperature(response) {
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
-    iconElement.setAttribute(
-        "src",
-        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
-    iconElement.setAttribute(
-        "alt",
-        response.data.weather[0].description
-    );
+    //iconElement.setAttribute(
+     //   "src",
+       // `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    //);
+    //iconElement.setAttribute(
+    //    "alt",
+     //   response.data.weather[0].description
+   // );
     getForecast(response.data.coord);
 }
 
@@ -158,4 +149,3 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("Melbourne");
-displayForecast();
