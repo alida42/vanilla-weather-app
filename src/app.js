3da80b064176fb7c1afbc318936b87324bdedddd
alida <101364345+alida42@users.noreply.github.com>
2022-06-21 15:07:34 +0200

    Weekly forecast moved to JS file

--- src/app.js
+++ src/app.js
@@ -31,41 +31,82 @@ function formatDay(timestamp) {
     return days[day];
 }
 
-function displayForecast(response) {
-    let forecast = response.data.daily;
 
+function displayForecast () {
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = `<div class="row">`;
-    forecast.forEach(function (forecastDay, index) {
-        if (index < 6) {
-            forecastHTML = 
-            forecastHTML + `
-      <div class="col-2">
-        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-        <img
-          src="http://openweathermap.org/img/wn/${
-            forecastDay.weather[0].icon
-          }@2x.png"
-          alt=""
-          width="42"
-        />
-        <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${Math.round(
-            forecastDay.temp.max
-          )}° </span>
-          <span class="weather-forecast-temperature-min"> ${Math.round(
-            forecastDay.temp.min
-          )}° </span>
-        </div>
-      </div>
-  `;
-        }
-    });
-
-    forecastHTML = forecastHTML + `</div>`;
+    let days = [`Thu`, `Fri`, `Sat`, `Sun`];
+    days.forEach(function(day)
+    {
+        forecastHTML = 
+                forecastHTML +  
+                `
+                    <div class="col-2">
+                        <div class="weekly-forecast-date">
+                            ${day}
+                        </div>
+                            <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Cloudy" width="40" />
+                        <div class="weekly-forecast-temperatures">
+                                <span class="weekly-forecast-temperature-max">
+                                    18°
+                                </span>
+                                <span class="weekly-forecast-temperature-min">
+                                    12°
+                                </span>
+                        </div>
+                    </div>
+                `;
+    })
+        forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
 }
+
+
+
+
+
+
+
+
+
+
+
+// function displayForecast(response) {
+//     let forecast = response.data.daily;
+
+ //    let forecastElement = document.querySelector("#forecast");
+
+//     let forecastHTML = `<div class="row">`;
+ //   forecast.forEach(function (forecastDay, index) {
+ //        if (index < 6) {
+//             forecastHTML = 
+//             forecastHTML + `
+ //      <div class="col-2">
+  //       <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+  //       <img
+ //          src="http://openweathermap.org/img/wn/${
+  //           forecastDay.weather[0].icon
+ //          }@2x.png"
+ //          alt=""
+ //          width="42"
+   //      />
+  //       <div class="weather-forecast-temperatures">
+  //         <span class="weather-forecast-temperature-max"> ${Math.round(
+   //          forecastDay.temp.max
+   //        )}° </span>
+   //        <span class="weather-forecast-temperature-min"> ${Math.round(
+   //          forecastDay.temp.min
+   //        )}° </span>
+    //     </div>
+   //   </div>
+  // `;
+  //       }
+ //    });
+
+  //   forecastHTML = forecastHTML + `</div>`;
+   //  forecastElement.innerHTML = forecastHTML;
+// }
 
 function getForecast(coordinates) {
     let apiKey = `d59d21823b0a48106ca99bf9e0c71303`;
@@ -117,3 +158,4 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("Melbourne");
+displayForecast();
